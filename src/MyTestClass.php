c395a42bee37e0f2ae5d6fd0421839def856aa09
Mike Decker <pookmish@gmail.com>
2019-09-16 15:38:48 -0700

    added file to generate coverage

---
+++ src/MyTestClass.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\stanford_text_editor;
+
+class MyTestClass {
+
+  public function getId() {
+    return 'Hey';
+  }
+
+  public function getLabel() {
+    return 'Foo';
+  }
+
+}
