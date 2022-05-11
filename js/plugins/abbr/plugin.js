1b9f0575a3c4bf8302a7756ee2a22f533994fabf
Ian Monroe <ian@ianmonroe.com>
2022-05-11 17:00:39 -0600

    Updated to better version

--- js/plugins/abbr/plugin.js
+++ js/plugins/abbr/plugin.js
@@ -33,6 +33,24 @@ CKEDITOR.plugins.add( 'abbr', {
 			toolbar: 'insert'
 		});
 
+		if ( editor.contextMenu ) {
+
+			// Add a context menu group with the Edit Abbreviation item.
+			editor.addMenuGroup( 'abbrGroup' );
+			editor.addMenuItem( 'abbrItem', {
+				label: 'Edit Abbreviation',
+				icon: this.path + 'icons/abbr.png',
+				command: 'abbr',
+				group: 'abbrGroup'
+			});
+
+			editor.contextMenu.addListener( function( element ) {
+				if ( element.getAscendant( 'abbr', true ) ) {
+					return { abbrItem: CKEDITOR.TRISTATE_OFF };
+				}
+			});
+		}
+
 		// Register our dialog file -- this.path is the plugin folder path.
 		CKEDITOR.dialog.add( 'abbrDialog', this.path + 'dialogs/abbr.js' );
 	}
